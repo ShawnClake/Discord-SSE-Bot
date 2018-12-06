837f2f08c733cc7448646bcba60798e9fc53a867
Shawn <shawn.clake@gmail.com>
2018-12-06 13:36:09 -0600

    Better fork bomb

--- urssediscord/commands/Fork.py
+++ urssediscord/commands/Fork.py
@@ -1,6 +1,9 @@
 class Fork:
     @staticmethod
     async def fork(ctx):
+        name = ctx.message.content.replace('/fork', '').replace(' ', '')
         for i in range(0, 5):
-            await ctx.bot.say(ctx.message.author.mention + " you're being fork bombed")
+            if len(name) < 1:
+                name = ctx.message.author.mention
+            await ctx.bot.say(name + " you're being fork bombed")
         await ctx.bot.say("The bot makers have planned for this and took the forks away")
