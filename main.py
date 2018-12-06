0c3b4d1715df2a7c9a62bb09c6a859e2b6998e25
Shawn <shawn.clake@gmail.com>
2018-12-06 13:17:44 -0600

    Bug fix

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ module.
 There are a number of utility commands being showcased here.'''
 bot = commands.Bot(command_prefix='/', description=description)
 
+
 @bot.event
 async def on_ready():
     print('Logged in as')
@@ -20,23 +21,27 @@ async def on_ready():
     print(bot.user.id)
     print('------')
 
+
 @bot.command(pass_context=True)
 async def role(ctx):
     await AssignRole.role(ctx)
+
 
 @bot.command(pass_context=True)
 async def so(ctx):
     await StackOverflow.so(ctx)
 
+
 @bot.event
 async def on_message(message):
-
     if message.author.name != "UR SSE Bot":
         await ImageBomb.bomb(message, bot)
         await bot.process_commands(message)
 
+
 @bot.event
 async def on_member_join(user):
     user.send("Welcome to our discord server! Give yourself roles by typing /role in any text channel.")
+
 
 bot.run(config.app.token)
