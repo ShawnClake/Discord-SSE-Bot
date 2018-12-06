109281b7f22ceaf15d2f4af74782ffea22fbffdc
Quinn Bast <Quinn.Bast@hotmail.com>
2018-12-05 22:21:58 -0600

    Sends users a message after they join the channel telling them how to add roles.

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import discord
 from discord.ext import commands
-import random
 from urssediscord.commands.AssignRole import AssignRole
 from urssediscord.commands.StackOverflow import StackOverflow
 from urssediscord.events.ImageBomb import ImageBomb
@@ -67,58 +65,8 @@ async def on_message(message):
     if any(string in msg for string in craig_terms):
         await ImageBomb.craig_bomb(message, bot)
 
-
-@bot.command()
-async def add(left: int, right: int):
-    """Adds two numbers together."""
-    await bot.say(left + right)
-
-
-@bot.command()
-async def roll(dice: str):
-    """Rolls a dice in NdN format."""
-    try:
-        rolls, limit = map(int, dice.split('d'))
-    except Exception:
-        await bot.say('Format has to be in NdN!')
-        return
-
-    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
-    await bot.say(result)
-
-
-@bot.command(description='For when you wanna settle the score some other way')
-async def choose(*choices: str):
-    """Chooses between multiple choices."""
-    await bot.say(random.choice(choices))
-
-
-@bot.command()
-async def repeat(times: int, content='repeating...'):
-    """Repeats a message multiple times."""
-    for i in range(times):
-        await bot.say(content)
-
-
-@bot.command()
-async def joined(member: discord.Member):
-    """Says when a member joined."""
-    await bot.say('{0.name} joined in {0.joined_at}'.format(member))
-
-
-@bot.group(pass_context=True)
-async def cool(ctx):
-    """Says if a user is cool.
-    In reality this just checks if a subcommand is being invoked.
-    """
-    if ctx.invoked_subcommand is None:
-        await bot.say('No, {0.subcommand_passed} is not cool'.format(ctx))
-
-
-@cool.command(name='bot')
-async def _bot():
-    """Is the bot cool?"""
-    await bot.say('Yes, the bot is cool.')
-
+@bot.event
+async def on_member_join(user):
+    user.send("Welcome to our discord server! Give yourself roles by typing /role in any text channel.")
 
 bot.run(config.app.token)
