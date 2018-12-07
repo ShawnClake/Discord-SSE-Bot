6d28360823cb80367fc807bdc4a4c79b83b7a040
Quinn Bast <Quinn.Bast@hotmail.com>
2018-12-07 14:51:56 -0600

    Updated channel creation

--- urssediscord/events/OnJoin.py
+++ urssediscord/events/OnJoin.py
@@ -29,27 +29,37 @@ class OnJoin:
         # Get all channels that are in the self-management category.
         channels = [channel for channel in bot.get_all_channels() if channel.category and channel.category.name == "Self-Management"]
         # Get all members in the discord who are not admins.
-        members = [member for member in bot.get_all_members() if member.top_role.name != "Admins"]
+        members = [member for member in bot.get_all_members() if member.top_role.name != "UR SSE Bot"]
 
-        # Loop all non-admins
-        for member in members:
-            # Check if the user has their own channel
-            self_channel = [channel for channel in channels if channel.permissions_for(member).send_messages]
+        # Loop through all guilds that the bot is registered to
+        # This ensures if players joined the guild while the bot was offline it creates a channel for them.
+        for guild in bot.guilds:
 
-            if not self_channel:
-                # Get the server
-                guild = bot.guilds[0]
+            # Loop all non-admins
+            for member in members:
+                # Check if the user has their own channel
+                self_channel = [channel for channel in channels if channel.topic == str(member.id)]
 
-                # Get the self-management category to add text channels to.
-                categories = guild.categories
-                self_management = [channel for channel in categories if channel.name == "Self-Management"][0]
+                if not self_channel:
+                    # Get the self-management category to add text channels to.
+                    categories = guild.categories
+                    self_management = [channel for channel in categories if channel.name == "Self-Management"]
 
-                # Generate the permissions for the channel
-                overwrites = {
-                    guild.default_role: PermissionOverwrite(read_messages=False),
-                    member: PermissionOverwrite(read_messages=True, send_messages=True)
-                }
+                    # If it doesn't exist, delete the category.
+                    if self_management:
+                        self_management = self_management[0]
+                    else:
+                        self_management = await guild.create_category("Self-Management")
 
-                # Create the private channel for the user
-                await guild.create_text_channel(str(member), overwrites=overwrites,
-                                                          category=self_management)
+                    # Generate the permissions for the channel
+                    overwrites = {
+                        guild.default_role: PermissionOverwrite(read_messages=False),
+                        member: PermissionOverwrite(read_messages=True, send_messages=True)
+                    }
+
+                    # Create the private channel for the user
+                    channel = await guild.create_text_channel(str(member),
+                                                    overwrites=overwrites,
+                                                    category=self_management)
+
+                    await channel.edit(topic=str(member.id))
